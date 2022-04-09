adb9a84f725ae0aa1548819b4ce804e2c9af73c3
Maik Schmidt <maik.schmidt.hl@gmail.com>
2022-04-09 22:23:31 +0200

    fix: fix due to "NONE" as AuthorizationType

--- src/4.bin/aws/api-gateway/api.ts
+++ src/4.bin/aws/api-gateway/api.ts
@@ -4,7 +4,8 @@ import { createContext, defaultConfig } from '../../../3.infrastructure/api/api'
 
 const server = new ApolloServer({
     ...defaultConfig,
-    context: ({ event }) => createContext(event.requestContext.authorizer.claims.sub)
+    // context: ({ event }) => createContext(event.requestContext.authorizer.claims.sub) // AuthorizationType.COGNITO
+    context: () => createContext('1337')
 });
 
 exports.handler = server.createHandler();
